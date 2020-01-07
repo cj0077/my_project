17b02b4350f509370446d08413a76ddf48f068fd
unknown <chandanjha0077@gmail.com>
2020-01-07 09:23:19 +0530

    Adding DB

--- first_program.py
+++ first_program.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unque_key=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+
+class Demo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), primary_key=True, nullable=False)
+    email = db.Column(db.String(120), unique_key=True, nullable=False)
+    mobile = db.Column(db.String(10), unique_key=True, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+
 
 posts = [
     {
